3db3be37678a5e1eff3b5ec27b75455d94b91ca0
Michael Liang <liangmicha@gmail.com>
2019-05-04 14:05:42 -0400

    Adding home page

---
+++ alt-comm-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-Title"> Alt Communucation App</div>
+    </div>
+  );
+}
+
+export default App;
